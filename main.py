65d668dc27eb7a06d8fb3a0bea096b98c0384780
Seth Dudenhofer <sdudenhofer@gmail.com>
2024-09-30 09:31:01 -0700

    adding logic

--- main.py
+++ main.py
@@ -8,4 +8,7 @@ engine = create_engine('postgresql://seth:G0thl0rd1@localhost:5432/city_data')
 data = pd.read_csv('Data/city_data.csv')
 df = pd.DataFrame(data)
 # TODO transform data so each entry has only one date
+if df.columns.str.contains('20').any is True:
+    df['Month/Year'] = pd.to_datetime(df.columns)
+
 df.to_sql(name='city_data', con=engine)
