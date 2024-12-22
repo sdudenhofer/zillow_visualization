da178ac73dda5f2bd19871ae19470dc4d81ad960
Seth Dudenhofer <sdudenhofer@gmail.com>
2024-12-22 15:41:03 -0800

    change looking for datetime format

--- initial.py
+++ initial.py
@@ -5,5 +5,5 @@ data = pl.read_csv('Data/city_data.csv')
 df = pl.DataFrame(data)
 
 for value in df.columns:
-    if type(value) == de.datetime:
+    if type(value) is de.datetime:
         print(value)
