eac8fee2d938df38bbe66b3187b91ccba0b6c44e
Seth Dudenhofer <sdudenhofer@gmail.com>
2024-12-22 08:44:51 -0800

    move to polars

--- initial.py
+++ initial.py
@@ -1,20 +1,9 @@
-import pandas as pd
-from datetime import datetime
+import polars as pl
+import datetime as de
 
-pd.options.display.float_format = '{:.2f}'.format
+data = pl.read_csv('Data/city_data.csv')
+df = pl.DataFrame(data)
 
-data = pd.read_csv('Data/city_data.csv')
-df = pd.DataFrame(data)
-
-new_df = pd.DataFrame()
 for value in df.columns:
-    if '20' in value:
-        new_value = datetime.strptime(value, '%Y-%m-%d').strftime('%Y-%m')
-        df.rename(columns={value: new_value}, inplace=True)
-        new_df['RegionID'] = df['RegionID']
-        new_df['Year'] = datetime.strptime(new_value, '%Y-%m').strftime('%Y')
-        new_df['Month'] = datetime.strptime(new_value, '%Y-%m').strftime('%m')
-        new_df['Price'] = df[new_value]
-
-print(new_df)
-            
+    if type(value) == de.datetime:
+        print(value)
