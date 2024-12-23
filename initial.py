1a250f4f3ff59d7b0d9e91c668965cf9aca83ea5
Seth Dudenhofer <sdudenhofer@gmail.com>
2024-12-22 17:15:37 -0800

    more updates

--- initial.py
+++ initial.py
@@ -1,9 +1,17 @@
 import polars as pl
-import datetime as de
+import datetime as dt
 
 data = pl.read_csv('Data/city_data.csv')
 df = pl.DataFrame(data)
 
+df_2000 = pl.DataFrame()
 for value in df.columns:
-    if type(value) is de.datetime:
-        print(value)
+    if '2000' in value:
+        dictionary = []
+        if value != dictionary:
+            new_date = dt.datetime.strptime(value, '%Y-%m-%d').strftime('%Y-%m')
+            df_2000 = df[['RegionID', 'SizeRank', 'RegionName', 'RegionType', 'StateName', 'State', 'Metro', 'CountyName', value]]
+        dictionary.append(value)
+
+print(df_2000)
+
