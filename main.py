ead28855b6876a496a672815f172a3bfe1e0f727
Seth Dudenhofer <sdudenhofer@gmail.com>
2025-01-09 16:07:52 -0800

    updated

--- main.py
+++ main.py
@@ -6,22 +6,25 @@ app = marimo.App(width="medium")
 
 @app.cell
 def _():
-    import polars as pl
+    import pandas as pd
     import altair as alt
 
-    df = pl.read_csv('Data/Metro_zhvi_uc_sfrcondo_tier_0.33_0.67_sm_sa_month.csv')
+    data = pd.read_csv('Data/Metro_zhvi_uc_sfrcondo_tier_0.33_0.67_sm_sa_month.csv')
 
-    df2000 = df[['RegionName', '2000-01-31', '2000-02-29', '2000-03-31', '2000-04-30', '2000-05-31', '2000-06-30', '2000-07-31', '2000-08-31', '2000-09-30', '2000-10-31', '2000-11-30', '2000-12-31']]
+    df = pd.DataFrame(data)
+    house_dates = [col for col in df if col.startswith('20')]
 
-    with pl.Config(auto_structify=True):
-        mean = df2000.with_columns(mean=(pl.col(['2000-01-31', '2000-02-29', '2000-03-31', '2000-04-30', '2000-05-31', '2000-06-30', '2000-07-31', '2000-08-31', '2000-09-30', '2000-10-31', '2000-11-30', '2000-12-31'])))
+    interested_cities = df[df['RegionName'].isin(["Eugene, OR"])]#, "Evansville, IN", "Nashville, TN", "Omaha, NE"])]
+    region = interested_cities['RegionName'].to_numpy()
 
-    alt.Chart(mean).mark_point(point=True).encode(
-        x='RegionName',
-        y='2000-01-31',
-        color='RegionName:N'
+    alt.Chart(interested_cities).mark_bar().encode(
+        alt.X("RegionName"),
+        alt.Y(alt.repeat("layer")),
+        color=alt.datum(alt.repeat("layer")),
+    ).repeat(
+        layer=['2023-01-31', '2023-02-28', '2023-03-31', '2023-04-30', '2023-05-31', '2023-06-30', '2023-07-31', '2023-08-31', '2023-09-30', '2023-10-31', '2023-11-30', '2023-12-31', '2024-01-31', '2024-02-29', '2024-03-31', '2024-04-30', '2024-05-31', '2024-06-30', '2024-07-31', '2024-08-31', '2024-09-30', '2024-10-31', '2024-11-30']
     )
-    return alt, df, df2000, mean, pl
+    return alt, data, df, house_dates, interested_cities, pd, region
 
 
 if __name__ == "__main__":
